ab9c52f8d99dba23af3302140b101bcbc1caec69
kavinduakash <bkkasamarasingha1220@gmail.com>
2025-10-24 14:22:23 +0530

    update mini student management

--- 04_MINI_STUDENT_MANAGEMENT/script.js
+++ 04_MINI_STUDENT_MANAGEMENT/script.js
@@ -1,61 +1,34 @@
-// ==================== Student DTO =======================
-
-class StudentDTO {
-    constructor(f_name, l_name, address) {
-        this._f_name=f_name;
-        this._l_name=l_name;
-        this._address=address;
-    }
-
-    get f_name() {
-        return this._f_name;
-    }
-
-    get l_name() {
-        return this._l_name;
-    }
-
-    get address() {
-        return this._address;
-    }
-
-    set f_name(f_name) {
-        this._f_name=f_name;
-    }
-
-    set l_name(l_name) {
-        this._l_name=l_name;
-    }
-
-    set address(address) {
-        this._address=address;
-    }
-
-}
+import StudentDTO from "./StudentDTO.js";
+import student_db from "./DB.js";
 
 // ==================== Add Student =======================
 
-const add_student_record = (obj) => {
-    let tbl_row = `<tr> <td>${obj.f_name}</td> <td>${obj.l_name}</td> <td>${obj.address}</td> <tr>`;
-    $("#student_tbl_body").append(tbl_row);
+const loads_student_tbl = () => {
+    $("#student_tbl_body").empty();
+
+    student_db.map((obj, index) => {
+        let tbl_row = `<tr> <td>${obj.f_name}</td> <td>${obj.l_name}</td> <td>${obj.address}</td> <tr>`;
+        $("#student_tbl_body").append(tbl_row);
+    });
 }
 
 $("#student_save_btn").on("click", function () {
-    console.log("save btn clicked!!!")
     let f_name = $("#f_name").val();
     let l_name = $("#l_name").val();
     let address = $("#address").val();
 
     let student_obj = new StudentDTO(f_name, l_name, address);
-    add_student_record(student_obj);
+    student_db.push(student_obj);
+    loads_student_tbl();
 });
 
 // ==================== Select Student =======================
 
 $("#student_tbl_body").on('click', 'tr', function () {
-    tbl_row = $(this);
+    // tbl_row = $(this);
 
-    // let result = $(this).index();
+    tbl_row = $(this).index();
+
     let tds = $(this).children();
 
     let f_name = tds.eq(0).text(); // fname
