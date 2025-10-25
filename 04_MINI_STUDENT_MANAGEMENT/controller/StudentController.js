b54cec32effa9b05f36a158ee03e842cf7b5056f
kavinduakash <bkkasamarasingha1220@gmail.com>
2025-10-25 12:53:12 +0530

    update mini student management

---
+++ 04_MINI_STUDENT_MANAGEMENT/controller/StudentController.js
@@ -0,0 +1,67 @@
+import {add_student, delete_student, get_students, get_student_detail} from "../model/StudentModel.js";
+
+// ==================== Load Student Tbl =======================
+const loads_student_tbl = () => {
+    $("#student_tbl_body").empty();
+
+    let student_list = get_students();
+
+    student_list.map((obj, index) => {
+        let tbl_row = `<tr> <td>${obj.f_name}</td> <td>${obj.l_name}</td> <td>${obj.address}</td> <tr>`;
+        $("#student_tbl_body").append(tbl_row);
+    });
+}
+
+// ==================== Add Student =======================
+$("#student_save_btn").on("click", function () {
+    let f_name = $("#f_name").val();
+    let l_name = $("#l_name").val();
+    let address = $("#address").val();
+
+    add_student(f_name, l_name, address);
+
+    loads_student_tbl();
+});
+
+// ==================== Select Student =======================
+
+$("#student_tbl_body").on('click', 'tr', function () {
+    console.log($(this).index())
+    tbl_row = $(this).index();
+
+    let student_obj = get_student_detail(tbl_row);
+
+    $("#f_name").val(student_obj.f_name);
+    $("#l_name").val(student_obj.l_name);
+    $("#address").val(student_obj.address);
+});
+
+// ==================== Remove Student =======================
+
+$("#student_delete_btn").on('click', () => {
+
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+            delete_student(tbl_row);
+            loads_student_tbl();
+            $("#student_reset_btn").click();
+
+            Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+            });
+        }
+    });
+});
+
+let tbl_row;
---
+++ 04_MINI_STUDENT_MANAGEMENT/model/StudentModel.js
@@ -0,0 +1,27 @@
+import student_db from "../db/DB.js";
+import StudentDTO from "../dto/StudentDTO.js";
+
+// ================ Add Student ===================
+const add_student = (f_name, l_name, address) => {
+    let student_obj = new StudentDTO(f_name, l_name, address);
+    student_db.push(student_obj);
+}
+
+// ============== Delete Student ==================
+const delete_student = (index) => {
+    student_db.splice(index, 1);
+}
+// =============== Get Students ===================
+const get_students = () => {
+    return student_db;
+}
+
+// =============== Get Students ===================
+const get_student_detail = (index) => {
+    return student_db[index];
+}
+
+// ============== Update Student ==================
+
+
+export {add_student, delete_student, get_students, get_student_detail};
